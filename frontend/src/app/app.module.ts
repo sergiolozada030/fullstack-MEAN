5489f2c43fe86ed83e1f8b92bf1a3811df7dae60
Sergio Andrés Lozada Perea <sergio.lozada@pragma.com.co>
2023-04-13 19:04:09 -0500

    estructura inicial

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { PrimengModule } from './shared/primeng/primeng.module';
+import { FormComponent } from './shared/components/form/form.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, PrimengModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
